e7ab2e7ec3873167797839c995f6a7cbb8437b94
erika613 <68814304+wack63@users.noreply.github.com>
2025-04-11 11:18:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // 如需在 renderer 使用 Node.js API，可打開以下兩行
+      // nodeIntegration: true,
+      // contextIsolation: false,
+    },
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:3000');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, 'build', 'index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
